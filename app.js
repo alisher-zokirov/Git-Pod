4ef34a2ac6c0971361d38d6b2c23e587b5fd8a4d
zokirov-alisher <zokirovalisher802@gmail.com>
2024-02-24 08:01:46 +0500

    main

---
+++ app.js
@@ -0,0 +1,19 @@
+function login(){
+    alert("Bu havola eskirgan")
+}
+
+function trynow(){
+    alert("Linkni olish uchun saytni oziga kiring❗")
+}
+
+function foxes(){
+    alert("Saytni linki eskirgan❗")
+}
+
+function frame(){
+    alert("Saytni linki eskirgan❗")
+}
+
+function kub(){
+    alert("Saytni linki eskirgan❗")
+}
